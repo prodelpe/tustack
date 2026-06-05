8debd05d2f5e3d020cdad637caef5f1061b433ca
Pau <pau@creagia.com>
2026-06-05 09:23:38 +0200

    Add Adzuna API config and env keys

---
+++ config/adzuna.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'app_id' => env('ADZUNA_APP_ID'),
+    'app_key' => env('ADZUNA_APP_KEY'),
+    'base_url' => 'https://api.adzuna.com/v1/api/jobs',
+    'country' => 'es',
+    'results_per_page' => 50,
+];
