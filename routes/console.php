e7fddb0f833615ef60473b7714d07b77e04c85c5
Pau <pau@creagia.com>
2026-06-13 13:03:36 +0200

    Disable Jooble scheduler until API quota is increased

--- routes/console.php
+++ routes/console.php
@@ -9,5 +9,5 @@ Artisan::command('inspire', function () {
 })->purpose('Display an inspiring quote');
 
 Schedule::job(new App\Jobs\FetchSourceJob('adzuna'))->dailyAt('00:00');
-Schedule::job(new App\Jobs\FetchSourceJob('jooble'))->dailyAt('00:00');
+// Schedule::job(new App\Jobs\FetchSourceJob('jooble'))->dailyAt('00:00'); // disabled: API quota exhausted
 Schedule::job(new App\Jobs\FetchSourceJob('tecnoempleo'))->dailyAt('00:00');
