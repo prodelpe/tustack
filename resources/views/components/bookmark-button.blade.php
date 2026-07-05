a8bcdee550cde356a8c9b6658cd53c642840e6fa
Pau <pau@creagia.com>
2026-07-05 11:43:22 +0200

    Fix bookmark button title evaluated as PHP instead of Alpine

--- resources/views/components/bookmark-button.blade.php
+++ resources/views/components/bookmark-button.blade.php
@@ -18,7 +18,7 @@
 >
     <x-outline-button
         @click="toggle"
-        :title="saved ? 'Remove from My companies' : 'Save to My companies'"
+        x-bind:title="saved ? 'Remove from My companies' : 'Save to My companies'"
         class="p-2"
     >
         <svg x-show="!saved" class="w-4 h-4 text-gray-400 dark:text-slate-500" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
