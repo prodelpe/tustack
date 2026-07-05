206fbd815e731baea1bcaeebf4d7969919d26788
Pau <pau@creagia.com>
2026-07-05 21:59:05 +0200

    Disable legacy FetchSourceJob crons pending new fetch system deploy

--- routes/console.php
+++ routes/console.php
@@ -8,7 +8,7 @@ Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
 
-Schedule::job(new App\Jobs\FetchSourceJob('adzuna'))->dailyAt('00:00');
+// Schedule::job(new App\Jobs\FetchSourceJob('adzuna'))->dailyAt('00:00');
 // Schedule::job(new App\Jobs\FetchSourceJob('jooble'))->dailyAt('00:00'); // disabled: API quota exhausted
-Schedule::job(new App\Jobs\FetchSourceJob('tecnoempleo'))->dailyAt('00:00');
+// Schedule::job(new App\Jobs\FetchSourceJob('tecnoempleo'))->dailyAt('00:00');
 Schedule::command('searches:notify')->dailyAt('08:00');
